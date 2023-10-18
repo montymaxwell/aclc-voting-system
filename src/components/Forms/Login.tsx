18c615e9918e1ee2530b449af15d7e1564d7761e
James Phillip Mayor <117265110+JpMayor1@users.noreply.github.com>
2023-10-18 11:24:48 +0800

    hehe

--- src/components/Forms/Login.tsx
+++ src/components/Forms/Login.tsx
@@ -1,122 +1,129 @@
-'use client'
-import './forms.css'
+"use client";
+import "./forms.css";
 
-import api from '@/lib/api'
-import { User } from '@/lib/types'
-import { LoginForm } from '@/app/api/types'
+import api from "@/lib/api";
+import { User } from "@/lib/types";
+import { LoginForm } from "@/app/api/types";
 
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { useUserStore } from '@/lib/UserStore'
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useUserStore } from "@/lib/UserStore";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import BeatLoader from "react-spinners/BeatLoader";
-import Image from 'next/image'
+import Image from "next/image";
 
 function Login() {
-  const router = useRouter()
-  const UserStore = useUserStore()
+    const router = useRouter();
+    const UserStore = useUserStore();
 
-  const [USN, setUSN] = useState<string | null>(null)
-  const [password, setPassword] = useState<string | null>(null)
-  const [isloading, setIsLoading] = useState(false);
+    const [USN, setUSN] = useState<string | null>(null);
+    const [password, setPassword] = useState<string | null>(null);
+    const [isloading, setIsLoading] = useState(false);
 
-  const Submit = async () => {
-    if (USN === null) {
-      return toast.error("USN is required");
-    }
+    const Submit = async () => {
+        if (USN === null) {
+            return toast.error("USN is required");
+        }
 
-    if (password === null) {
-      return toast.error("Password is required");
-    }
+        if (password === null) {
+            return toast.error("Password is required");
+        }
 
-    setIsLoading(true);
+        setIsLoading(true);
 
-    const res = await api('auth').post<LoginForm>({ USN, password });
-    if (res.state === true) {
-      const user: User = res.data;
+        const res = await api("auth").post<LoginForm>({ USN, password });
+        if (res.state === true) {
+            const user: User = res.data;
 
-      UserStore.addUserInfo({
-        usn: user.USN,
-        strand: user.strand,
-        role: user.role!,
-        voted: user.voted!,
-        voteList: user.voteList!,
-      });
+            UserStore.addUserInfo({
+                usn: user.USN,
+                strand: user.strand,
+                role: user.role!,
+                voted: user.voted!,
+                voteList: user.voteList!,
+            });
 
-      switch (user.role) {
-        case 'user':
-          router.push('/voting');
+            switch (user.role) {
+                case "user":
+                    router.push("/voting");
 
-          break;
+                    break;
 
-        case 'admin':
-          router.push('/users');
-          break;
-      }
-
-    } else {
-      toast.error(res.message);
-      setIsLoading(false);
-    }
-  }
-
-  return (
-    <div className="w-full flex flex-col">
-      <div className='w-full my-8 text-center'>
-        <div className="w-auto my-3">
-          <Image src='/aclclogo.png' className='mx-auto' alt='aclclogo' width={120} height={120} />
-        </div>
-        <h1 className='text-7xl font-bold text-blue-900'>ACLC</h1>
-        <h2 className='text-xl font-semibold text-blue-900'>College of Commonwealth</h2>
-        <h6 className='text-lg mt-2'>SSC Voting System</h6>
-      </div>
-      <div className='w-full flex flex-col my-2 px-5'>
-        <label htmlFor="USN" className='form-label mx-3 my-2'>USN</label>
-        <input
-          type="text"
-          name='USN'
-          placeholder='USN'
-          className='text-input primary-input'
-          onChange={(ev) => {
-            if (ev.target.value === '') {
-              setUSN(null);
+                case "admin":
+                    router.push("/users");
+                    break;
             }
-            else {
-              setUSN(ev.target.value);
-            }
-          }}
-        />
-      </div>
-      <div className='w-full flex flex-col my-2 px-5'>
-        <label htmlFor="Password" className='form-label mx-3 my-2'>Password</label>
-        <input
-          type="password"
-          name='password'
-          placeholder='Password'
-          className='text-input primary-input'
-          onChange={(ev) => {
-            if (ev.target.value === '') {
-              setPassword(null);
-            }
-            else {
-              setPassword(ev.target.value);
-            }
-          }}
-        />
-      </div>
-      <div className='w-full flex flex-col my-8 px-5'>
-        <button className='button success' onClick={Submit}>{isloading ? (
-          <BeatLoader color="#ffffff" />
-        ) : (
-          "Login"
-        )}</button>
-      </div>
-      {/* <footer className='w-full mt-10 mx-5 text-gray-500'>
+        } else {
+            toast.error(res.message);
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <div className="w-full flex flex-col">
+            <div className="w-full my-8 text-center">
+                <div className="w-auto my-3">
+                    <Image
+                        src="/aclclogo.png"
+                        className="mx-auto"
+                        alt="aclclogo"
+                        width={80}
+                        height={80}
+                    />
+                </div>
+                <h1 className="text-7xl font-bold text-blue-900">ACLC</h1>
+                <h2 className="text-xl font-semibold text-blue-900">
+                    College of Commonwealth
+                </h2>
+                <h6 className="text-lg mt-2">SSC Voting System</h6>
+            </div>
+            <div className="w-full flex flex-col my-2 px-5">
+                <label htmlFor="USN" className="form-label mx-3 my-2">
+                    USN
+                </label>
+                <input
+                    type="text"
+                    name="USN"
+                    placeholder="USN"
+                    className="text-input primary-input"
+                    onChange={(ev) => {
+                        if (ev.target.value === "") {
+                            setUSN(null);
+                        } else {
+                            setUSN(ev.target.value);
+                        }
+                    }}
+                />
+            </div>
+            <div className="w-full flex flex-col my-2 px-5">
+                <label htmlFor="Password" className="form-label mx-3 my-2">
+                    Password
+                </label>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="text-input primary-input"
+                    onChange={(ev) => {
+                        if (ev.target.value === "") {
+                            setPassword(null);
+                        } else {
+                            setPassword(ev.target.value);
+                        }
+                    }}
+                />
+            </div>
+            <div className="w-full flex flex-col my-8 px-5">
+                <button className="button success" onClick={Submit}>
+                    {isloading ? <BeatLoader color="#ffffff" /> : "Login"}
+                </button>
+            </div>
+            {/* <footer className='w-full mt-10 mx-5 text-gray-500'>
         <div>Hello</div>
       </footer> */}
-    </div>
-  )
+        </div>
+    );
 }
 
-export default Login
+export default Login;
